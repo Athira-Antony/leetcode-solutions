266709edd9a5f061b7eabbbff131868ee1ea80d7
Athira Antony <147913045+Athira-Antony@users.noreply.github.com>
2025-05-11 23:36:42 +0530

    Added new

---
+++ 1128)Number of Equivalent Domino Pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) 
+    {
+        int result = 0;
+        vector<int>count(100,0);
+        int val;
+        for(auto d: dominoes)
+        {
+            if(d[0] > d[1])
+                val = 10 * d[0] + d[1];
+            else
+                val = 10 * d[1] + d[0];
+            result += count[val]; // follows the equation n(n-1)/2 for pairs
+            count[val]++;
+        }
+        return result;
+    }
+};
