80e2622a7bc573dfe7a1726efe81007dbcb90db6
Athira Antony <147913045+Athira-Antony@users.noreply.github.com>
2025-05-15 22:38:03 +0530

    Soln

---
+++ 1584)minCosttoConnectAll.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) 
+    {
+        vector<int>dist(points.size(),INT_MAX);
+        int res = 0;
+
+        for(int i=0; i<points.size() - 1; i++)
+        {
+            for(int j = i+1; j<points.size(); j++)
+            {
+                dist[j] = min(dist[j] ,abs(points[i][0] - points[j][0]) +
+                                   abs(points[i][1] - points[j][1]));
+
+                if(dist[j] < dist[i+1])
+                {
+                    swap(points[j],points[i+1]);
+                    swap(dist[j],dist[i+1]);
+                }
+            }
+            res+=dist[i+1];
+        }
+        return res;
+    }
+};
